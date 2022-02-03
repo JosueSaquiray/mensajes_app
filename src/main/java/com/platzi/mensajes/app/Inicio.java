109a930186990cac5dc5b9d779d2902c33a5eba4
Josue Saquiray <josue.saquiray@pucp.edu.pe>
2022-02-03 16:05:22 -0500

    Primer commit

---
+++ src/main/java/com/platzi/mensajes/app/Inicio.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.platzi.mensajes.app;
+
+/**
+ *
+ * @author Hp
+ */
+public class Inicio {
+    public static void main(String[] args) {
+      
+    }
+}
